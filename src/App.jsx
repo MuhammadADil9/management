49bf9795d0778e3e1dc97bee0098980de6f45357
= <madilaslam9@gmail.com>
2024-06-30 23:49:28 +0500

    structured the folder layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './assets/App.css';
 import Header from './Header.jsx';
-import RouteerPath from './routes.jsx';
+import RouteerPath from '../routes/routes.jsx';
 
 function App() {
   return (
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Landing from './Landing.jsx';
-import Dashboard from './Dashboard.jsx';
-import Taskbar from './Taskbar.jsx';
-import Activities from './Activities.jsx';
-import NoRoute from './NoRoute.jsx';
-import Signup from './Signup.jsx';
-import TaskComponent from './task.jsx';
+import Landing from '../components/Landing.jsx';
+import Dashboard from '../components/Dashboard.jsx';
+import Taskbar from '../components/Taskbar.jsx';
+import Activities from '../components/Activities.jsx';
+import NoRoute from '../components/NoRoute.jsx';
+import Signup from '../components/Signup.jsx';
+import TaskComponent from '../components/task.jsx';
 
 const RouteerPath = () => {
   return (
