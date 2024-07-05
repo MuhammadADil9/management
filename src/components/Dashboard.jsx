6191230fec2672237beda6ffe445f67408323da5
= <madilaslam9@gmail.com>
2024-07-05 19:19:11 +0500

    Dashboard set for the activities

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,25 +3,29 @@ import Taskbar from "./Taskbar";
 import { useState } from "react";
 import { createContext } from "react";
 
-const taskContext = createContext()
+const taskContext = createContext();
 
 function Dashboard() {
 
-    const [task,SetTask] = useState([]);
-    const addTask = () => {
-        SetTask([...task,task])
-    }
+  const [task, SetTask] = useState("");
+  const [taskArr, settaskArr] = useState([])
 
-    return (
+  const addTask = () => {
+    settaskArr([...taskArr, task]);
+  };
+
+  return (
     <>
-      <Taskbar />
-    
+      <taskContext.Provider value={taskArr}>
+        <Taskbar />
+      </taskContext.Provider>
+
       <div>
         <h1>Schedule Your day</h1>
         <br />
         <label htmlFor="">Your Task is Awaiting</label>
-        <input type="text" name="task" id="task"/>
-        <button onChange={addTask} >Submit</button>
+        <input type="text" name="task" id="task" onChange={(e)=>SetTask(e.target.value)} />
+        <button onChange={addTask}>Submit</button>
       </div>
       <Outlet />
     </>
